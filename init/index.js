3a71e850c203141194ba55f5da548610c9c3d541
Pavel Šturc <psturc@redhat.com>
2019-07-11 10:10:29 +0200

    feat: script for registering devices

--- init/index.js
+++ init/index.js
@@ -3,8 +3,10 @@ const FormData = require('form-data')
 
 const upsInstanceUrl = process.env.UPS_URL || 'http://localhost:9999'
 const appsEndpoint = `${upsInstanceUrl}/rest/applications`
+const registerDeviceEndpoint = `${upsInstanceUrl}/rest/registry/device`
 
 const { iosBase64Cert, iosCertPassword } = require('../fixtures')
+const { generateDeviceToken } = require('../helpers')
 
 async function createApp (appName) {
   return axios({
@@ -53,19 +55,49 @@ async function createIosVariant (appId, variantName) {
   })
 }
 
-async function init () {
-  const application = await createApp('test')
-  const responseData = application.data
+async function registerDevice (variantId, variantSecret) {
+  axios({
+    url: `${registerDeviceEndpoint}`,
+    method: 'POST',
+    auth: {
+      username: variantId,
+      password: variantSecret
+    },
+    data: {
+      deviceToken: await generateDeviceToken(),
+      deviceType: 'tablet',
+      operatingSystem: 'Android',
+      osVersion: '6.1.2',
+      alias: 'cordova',
+      categories: ['football', 'sport']
+    }
+  }).catch((err) => {
+    console.log(err.response.status)
+    console.log(err.response.statusText)
+  }).then((res) => {
+    console.log(res.status)
+    console.log(res.statusText)
+  })
+}
 
-  await createAndroidVariant(responseData.pushApplicationID, 'test')
-  await createIosVariant(responseData.pushApplicationID, 'test')
+async function init () {
+  const application = (await createApp('test')).data
+
+  const androidVariant = (await createAndroidVariant(application.pushApplicationID, 'test')).data
+  const iosVariant = (await createIosVariant(application.pushApplicationID, 'test')).data
 
   console.log(`
-    Push Application ID:    ${responseData.pushApplicationID}
-    Master Secret:          ${responseData.masterSecret}
+    Push Application ID:    ${application.pushApplicationID}
+    Master Secret:          ${application.masterSecret}
+    Android Variant ID:     ${androidVariant.variantID}
+    Android Variant Secret: ${androidVariant.secret}
+    iOS Variant ID:         ${iosVariant.variantID}
+    iOS Variant Secret:     ${iosVariant.secret}
     `
   )
-  //await deleteApp(appId)
+  await registerDevice(androidVariant.variantID, androidVariant.secret)
+  await registerDevice(iosVariant.variantID, iosVariant.secret)
+  //await deleteApp(application.pushApplicationID)
 }
 
 init()
